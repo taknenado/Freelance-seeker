111d73c6eb2db0221b05305c0efcc5a215bc732d
taknenado <116570090+taknenado@users.noreply.github.com>
2024-02-14 17:52:49 +0300

    Главная страница пользователей
    После авторизации, нас перебросит сюда, будет аналогична index.php но, с некоторыми кнопками, таким как "Личный кабинет"

---
+++ user_page.php
@@ -0,0 +1,51 @@
+<?php
+require_once('auth_check.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Страница пользователя</title>
+    <style>
+    .button-container {
+        display: flex;
+        justify-content: flex-start;
+  }
+  
+  .logout_button {
+        display: inline-block;
+        background-color: #eb1639;
+        color: white;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        border-radius: 5px;
+        border: none;
+        cursor: pointer;
+        font-size: 16px;
+        transition: background-color 0.3s;
+        margin-top: 20px;
+  }
+  .userinfo_button {
+        display: inline-block;
+        background-color: #1E90FF;
+        color: white;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        border-radius: 5px;
+        border: none;
+        cursor: pointer;
+        font-size: 16px;
+        transition: background-color 0.3s;
+        margin-top: 20px;
+  }
+    </style>
+</head>
+<body>
+    <h1>Добро пожаловать, <?php echo $_SESSION['username']; ?>!</h1>
+    <p>Это специальная страница для авторизованных пользователей.</p>
+    <p>Здесь вы можете отображать контент, доступный только авторизованным пользователям.</p>
+    <a href="personal_info.php" class="userinfo_button">Личный кабинет</a>
+    <!-- Дополнительный контент для зарегистрированных пользователей -->
+</body>
+</html>
