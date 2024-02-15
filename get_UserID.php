92e202cd06fcef5b5a16112e3570d80a442209c1
taknenado <116570090+taknenado@users.noreply.github.com>
2024-02-16 00:05:56 +0300

    Удаляем лишнее и создаем простую проверки на аутентификацию пользователя
    Теперь, что бы получить user id, это делается отдельно в файле get_UserID.php и когда нужно, вызываем его.
    По факту, просто разделил auth_check.php на check_auth.php где происходит проверка на аутентификацию пользователя и get_User.php где мы достаем ID юзера когда нам нужно.

---
+++ get_UserID.php
@@ -0,0 +1,25 @@
+<?php
+require_once("DB_config.php");
+
+// Запрос к базе данных для получения user_id
+$query = "SELECT user_id FROM users WHERE username = ?";
+$stmt = mysqli_prepare($connection, $query);
+mysqli_stmt_bind_param($stmt, "s", $_SESSION['username']);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+
+// Проверка наличия результатов запроса
+if ($result && mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $user_id = $row['user_id'];
+
+    // Устанавливаем user_id в сессию
+    $_SESSION['user_id'] = $user_id;
+} else {
+    // Обработка случая, когда данные пользователя не найдены
+    echo "Данные пользователя не найдены.";
+}
+
+// Закрываем подготовленный запрос
+mysqli_stmt_close($stmt);
+?>
