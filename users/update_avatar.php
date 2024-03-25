4606d629e0741fcaf0808db02d5cea3042149caa
taknenado <116570090+taknenado@users.noreply.github.com>
2024-03-25 14:42:12 +0300

    Поставил более подходящую аватарку для сайта
    Теперь, если пользователь меняет свою аватарку, то прошлая удаляется с директории сайта, что бы не засорять диск.

--- users/update_avatar.php
+++ users/update_avatar.php
@@ -2,14 +2,16 @@
 
 require_once '../check_auth.php'; 
 require_once '../DB_config.php';
-
+require_once '../get_UserID.php';
+require_once '../site_settings.php';
+const DEFAULT_AVATAR = 'http://localhost/Freelance-seeker/uploads/avatars/default-avatar.png';
+$user = mysqli_fetch_assoc(mysqli_query($connection, "SELECT * FROM users WHERE user_id = $user_id"));
+$previousAvatar = $user['avatar_path'];
 // Проверка метода запроса
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-  // Получаем данные о файле
   $avatar = $_FILES['avatar'];
   
-  // Валидация файла
   $allowedTypes = ['image/jpeg', 'image/png'];
   $maxSize = 2 * 1024 * 1024; // 2 MB
   if (!in_array($avatar['type'], $allowedTypes)) {
@@ -21,17 +23,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // Если ошибок нет - сохраняем файл
   if (!isset($error)) {
     
-    // Папка для сохранения
     $uploadFolder = '../uploads/avatars/';
     
-    // Генерируем уникальное имя
     $fileExtension = pathinfo($avatar['name'], PATHINFO_EXTENSION);
     $newFileName = uniqid() . "." . $fileExtension;
 
-    // Загружаем файл на сервер 
     move_uploaded_file($avatar['tmp_name'], $uploadFolder . $newFileName);
     
-    // Успешный ответ
     $response = [
       'uploaded' => true,
       'fileName' => $newFileName
@@ -45,13 +43,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   }
   $user_id = $_SESSION['user_id'];
 
-// Обновляем путь к аватару в БД
 $sql = "UPDATE users 
         SET avatar_path = '/uploads/avatars/$newFileName' 
         WHERE user_id = $user_id";
 
 mysqli_query($connection, $sql);
-
+if($previousAvatar && $previousAvatar !== DEFAULT_AVATAR) {
+  if($previousAvatar) {
+    
+    if(!unlink(__DIR__ . '/../' . $previousAvatar)) {
+    }
+    
+  }
+}
 // Делаем редирект обратно
 header('Location: personal_info.php?avatar_updated');
 
