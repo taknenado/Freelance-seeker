ce7233a78fd124a77febce353b30a97eb166747b
taknenado <116570090+taknenado@users.noreply.github.com>
2024-04-01 12:05:44 +0300

    Сделал запись профессии в базу данных

--- users/save_profile.php
+++ users/save_profile.php
@@ -8,13 +8,34 @@ if(!isset($_SESSION['username']) && $_SERVER['SCRIPT_NAME'] != '/index.php') {
 }
 require_once("../DB_config.php");
 require_once("../get_UserID.php");
-
+$professions = array(
+  '0' => 'Выберите профессию',
+  '1' => 'Разработка сайтов',
+  '2' => 'Дизайн',
+  '3' => 'Арт',
+  '4' => 'Программирование',
+  '5' => 'Тексты',
+  '6' => 'Реклама/Маркетинг',
+  '7' => '3D Графика/Анимация',
+  '8' => 'Архитектура / Интерьеры / Инжиниринг',
+  '9' => 'Оптимизация (SEO)',
+  '10' => 'Менеджмент',
+  '11' => 'Флеш',
+  '12' => 'Переводы',
+  '13' => 'Фотография',
+  '14' => 'Аудио/Видео',
+  '15' => 'Консалтинг',
+  '16' => 'Другое',
+  '17' => 'Соцсети',
+  '18' => 'Бухгалтерия'
+);
 if(isset($_POST['phone'])) {
 
   $user_id = $_SESSION['user_id'];
 
   $phone = $_POST['phone'];
-  $profession = $_POST['profession'];
+  $professionValue = $_POST['profession'];
+  $profession = isset($professions[$professionValue]) ? $professions[$professionValue] : 'Другое';
   $specialization = $_POST['specialization'];
   
 
