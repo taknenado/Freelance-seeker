5bd6d749fcb7f2e8b65ac95b176620856ae6b9ed
taknenado <danelyana5@gmail.com>
2024-05-13 00:22:10 +0300

    Обновил базы данных

--- installation/02-create-table-users.sql
+++ installation/02-create-table-users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -9,12 +9,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_type` enum('F','E','A') NOT NULL,
   `avatar_path` varchar(255) DEFAULT '/uploads/avatars/default-avatar.png',
   `specialization` varchar(255) DEFAULT NULL,
-  `profession` varchar(255) DEFAULT NULL,
+  `profession` varchar(255) DEFAULT NULL
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `phone`, `gender`, `birthdate`, `user_type`, `avatar_path`, `specialization`, `profession`) VALUES
-(1, 'taknenado', 'test@test.test', '123', '+7 (543) 254-35-43', 'male', '2024-03-01', 'A', '/uploads/avatars/660c087527324.jpg', 'Продвижение сайта по позициям', 'Оптимизация (SEO)'),
-(2, '123', 'test1@test.test', '123', '+7 (323) 232-32-32', 'male', '2024-03-01', 'F', '/uploads/avatars/66015ec7a58a9.png', 'Продвижение сайта по трафику', 'Оптимизация (SEO)'),
-(3, '1234', 'test2@test.test', '123', '+7 (123) 123-12-33', 'male', '2024-03-01', 'F', '/uploads/avatars/660161bc439da.jpg', 'Крауд-маркетинг', 'Реклама/Маркетинг'),
-(4, '12345', 'test4@test.test', '123', '+7 (333) 333-32-32', 'male', '2024-03-02', 'F', '/uploads/avatars/66016267f206d.jpg', 'Продвижение на Youtube', 'Соцсети');
+(1, 'taknenado', 'test@test.test', '123', '+7 (543) 254-35-43', 'male', '2024-03-01', 'A', '/uploads/avatars/6641226e75032.png', 'Продвижение сайта по позициям', 'Оптимизация (SEO)'),
+(2, 'User', 'test1@test.test', '123', '+7 (323) 232-32-32', 'male', '2024-03-01', 'F', '/uploads/avatars/6641223c9159c.png', 'Продвижение сайта по трафику', 'Оптимизация (SEO)'),
+(3, 'Oleg', 'test2@test.test', '123', '+7 (123) 123-12-33', 'male', '2024-03-01', 'F', '/uploads/avatars/660161bc439da.jpg', 'Крауд-маркетинг', 'Реклама/Маркетинг'),
+(4, 'Влад', 'test3@test.test', '123', '+7 (333) 333-32-32', 'male', '2024-03-02', 'F', '/uploads/avatars/664121ec140be.jpg', 'Продвижение на Youtube', 'Соцсети'),
+(5, 'chachar1k', 'test4@test.test', '123', '+7 (337) 333-32-32', 'male', '2024-03-02', 'E', '/uploads/avatars/default-avatar.png', 'Продвижение на Youtube', 'Соцсети'),
+(6, 'Overloaded', 'hih@gmail.com', '123', '+7 (950) 572-66-83', 'male', '2024-04-15', 'F', '/uploads/avatars/default-avatar.png', NULL, NULL);
