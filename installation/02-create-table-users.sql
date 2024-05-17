e628799115f18fb67f5939b3fc59651817d8edc6
taknenado <116570090+taknenado@users.noreply.github.com>
2024-05-17 10:23:49 +0300

    Final update

--- installation/02-create-table-users.sql
+++ installation/02-create-table-users.sql
@@ -13,10 +13,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `phone`, `gender`, `birthdate`, `user_type`, `avatar_path`, `specialization`, `profession`) VALUES
-(1, 'taknenado', 'test@test.test', '123', '+7 (543) 254-35-43', 'male', '2024-03-01', 'A', '/uploads/avatars/6641226e75032.png', 'Продвижение сайта по позициям', 'Оптимизация (SEO)'),
-(2, 'User', 'test1@test.test', '123', '+7 (323) 232-32-32', 'male', '2024-03-01', 'F', '/uploads/avatars/6641223c9159c.png', 'Продвижение сайта по трафику', 'Оптимизация (SEO)'),
-(3, 'Oleg', 'test2@test.test', '123', '+7 (123) 123-12-33', 'male', '2024-03-01', 'F', '/uploads/avatars/660161bc439da.jpg', 'Крауд-маркетинг', 'Реклама/Маркетинг'),
-(4, 'Влад', 'test3@test.test', '123', '+7 (333) 333-32-32', 'male', '2024-03-02', 'F', '/uploads/avatars/664121ec140be.jpg', 'Продвижение на Youtube', 'Соцсети'),
-(5, 'chachar1k', 'test4@test.test', '123', '+7 (337) 333-32-32', 'male', '2024-03-02', 'E', '/uploads/avatars/default-avatar.png', 'Продвижение на Youtube', 'Соцсети'),
-(6, 'Overloaded', 'hih@gmail.com', '123', '+7 (950) 572-66-83', 'male', '2024-04-15', 'F', '/uploads/avatars/default-avatar.png', NULL, NULL);
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `birthdate`, `user_type`, `phone`, `gender`, `profession`, `specialization`, `avatar_path`) VALUES
+(1, 'taknenado', 'test@test.test', '123', '2024-03-01', 'A', '+7 (543) 254-35-43', 'male', 'Оптимизация (SEO)', 'Продвижение сайта по позициям', '/uploads/avatars/6647055952dcb.PNG'),
+(2, '123', 'test1@test.test', '123', '2024-03-01', 'F', '+7 (323) 232-32-32', 'male', 'Оптимизация (SEO)', 'Продвижение сайта по трафику', '/uploads/avatars/66470599166e4.PNG'),
+(4, '12345', 'test4@test.test', '123', '2024-03-02', 'F', '+7 (333) 333-32-32', 'male', 'Соцсети', 'Продвижение на Youtube', '/uploads/avatars/66016267f206d.jpg'),
+(5, 'АЛОООО', 'what@gmail.com', '123', '2024-02-22', 'F', '+7 (534) 543-53-45', 'male', NULL, NULL, '/uploads/avatars/default-avatar.png'),
+(9, 'taknenado_test', 'test123@test.test', '123', '2024-05-02', 'E', '+7 (323) 412-43-24', 'male', NULL, NULL, '/uploads/avatars/default-avatar.png'),
+(10, 'New_user', 'test9@mail.ru', '123', '2024-05-11', 'E', '+7 (432) 543-26-34', 'female', NULL, NULL, '/uploads/avatars/663491a2b73df.jpg'),
+(11, 'New_user1', '1875@gmail.ru', '123', '2024-04-26', 'F', '+7 (432) 423-52-35', 'male', NULL, NULL, '/uploads/avatars/default-avatar.png');
